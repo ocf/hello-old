5d5c89a62e212f507b5e2c3455fedb2629782966
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-01-20 15:32:00 -0800

    Add lab home page

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 compass compile
-scp -r index.html images fonts javascripts stylesheets death:/srv/sites/hello/
+scp -r index.html lab.html images fonts javascripts stylesheets death:/srv/sites/hello/
